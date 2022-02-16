cc5bf765adbce55ab8a7b12cc41da5ae42bd4452
oded azoulay <dogshouseisrael@gmail.com>
2022-02-17 00:51:32 +0200

    all types covered now lesson on compiler

--- app.js
+++ app.js
@@ -1,24 +1,12 @@
-function add(n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Max';
+// userName = userInput
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-// function who doesnt return anything is void
-function printResult(num) {
-    console.log('Result: ' + num);
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
 }
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-printResult(add(5, 12));
-// we can point to a function with a variable and make sure it will point to a function. 
-// let combineValues: Function;
-// more precise
-var combineValues;
-combineValues = add;
-console.log(combineValues(8, 8));
-// tests:
-// combineValues = 5;
-// combineValues = printResult;
-addAndHandle(10, 20, function (result) {
-    console.log(result);
-});
+generateError('An Error occurred!', 500);
